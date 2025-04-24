1bd567ec86d8cf48cf7e0aeb8d66d1068dc2b01d
routhleck <sichaohe@stu.pku.edu.cn>
2025-04-24 15:14:32 +0800

    Remove python 3.9 from publish ci

--- setup_cuda.py
+++ setup_cuda.py
@@ -172,7 +172,7 @@ setup(
     },
     install_requires=['brainunit', 'brainstate', 'jax', 'jaxlib', 'taichi'],
     extras_require={"test": "pytest"},
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     url='https://github.com/chaoming0625/braintaichi',
     ext_modules=[
         Extension("gpu_ops",
@@ -191,7 +191,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
